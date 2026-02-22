0a36bc028905bdcd37f96dc5a6889da369cfe050
duongngoctuan571-jpg <duongngoctuan571@gmail.com>
2026-02-22 18:02:53 +0700

    Add game-specific loading scripts for TanPhathub

---
+++ TanPhathub.lua
@@ -0,0 +1,17 @@
+local idplace = game.PlaceId
+
+if idplace == 73956553001240 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Zayn31214/name/refs/heads/main/SterlingNew"))()
+elseif idplace == 85207102870777 or idplace == 12137249458 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/duongngoctuan571-jpg/FPSGun/main/TanPhatAim.lua"))()	
+elseif idplace == 85211729168715 or idplace == 79091703265657 or idplace == 100117331123089 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/duongngoctuan571-jpg/TongHop/refs/heads/main/TanPhatHub.lua"))()
+elseif idplace == 98502499119821 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/bvect1037-alt/KhfreshHub/refs/heads/main/HeavyWeightFishing"))()
+elseif idplace == 119579217517090 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/duongngoctuan571-jpg/hehe/refs/heads/main/dell-co-key"))()
+elseif idplace == 16281300371 then 
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/main/Loader.lua"))()
+else
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+end
